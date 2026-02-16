e166b0d438322e59dfe5cef7bb7d2a20bc5aa24b
chillead <fres1x43@gmail.com>
2026-02-16 19:45:29 +0500

    deploy: create server/utils/keys.js

---
+++ server/utils/keys.js
@@ -0,0 +1,13 @@
+export const K={
+  user:(uid)=>`user:${uid}`,
+  userByEmail:(email)=>`userByEmail:${email}`,
+  userByUsername:(u)=>`userByUsername:${u}`,
+  code:(email)=>`code:${email}`,
+  chat:(id)=>`chat:${id}`,
+  chatMsgs:(id)=>`chat:${id}:msgs`,
+  chatLast:(id)=>`chat:${id}:last`,
+  userChats:(uid)=>`u:${uid}:chats`,
+  unread:(uid,chatId)=>`u:${uid}:unread:${chatId}`,
+  post:(id)=>`post:${id}`,
+  postLikes:(id)=>`post:${id}:likes`,
+};
