0f2213b40ea20463d8e7ecafbf6a905ccbf17a04
chillead <fres1x43@gmail.com>
2026-02-20 22:34:19 +0500

    wipe: delete public/lib/dom.js

--- public/lib/dom.js
+++
@@ -1,3 +0,0 @@
-export const $=(id)=>document.getElementById(id);
-export const qsa=(sel,root=document)=>[...root.querySelectorAll(sel)];
-export function esc(s){return String(s??"").replaceAll("&","&amp;").replaceAll("<","&lt;").replaceAll(">","&gt;").replaceAll('"',"&quot;").replaceAll("'","&#039;");}
